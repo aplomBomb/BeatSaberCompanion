b7b84836a26a940542b9f5ffe81ea6e04af1f93f
bomb <seizethesteez@gmail.com>
2018-11-03 20:56:27 -0400

    heroku server init

--- src/components/TwitchStreamData.js
+++ src/components/TwitchStreamData.js
@@ -8,6 +8,10 @@ class TwitchStreamData extends Component {
   };
 
   async componentDidMount() {
+    // const accessToken = await axios.get(
+    //   "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=zvk3ma5y06cj6l7yud71ratb0olwt2&redirect_uri=https://localhost/"
+    // );
+
     const twitchAuthData = await axios.post(process.env.REACT_APP_TWITCH_POST);
     const twitchToken = twitchAuthData.data.access_token;
     // console.log(twitchToken);
@@ -21,7 +25,7 @@ class TwitchStreamData extends Component {
     this.setState({
       streamData: twitchFetchData.data.data
     });
-    // console.log(this.state.streamData);
+    console.log(this.state.streamData);
   }
 
   render() {
